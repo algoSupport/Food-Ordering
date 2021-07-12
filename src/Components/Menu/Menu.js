a54ffc16554c6015c71f8385e75fab7553b59e85
algoSupport <algoplusnine@outlook.com>
2021-07-12 20:36:01 +0500

    Updated icon

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,64 +1,78 @@
 import CardNew from "./../../UI/Card/Card";
-import {Grid,Typography,Container, Box,Paper,Card} from "@material-ui/core"
+import {
+  Grid,
+  Typography,
+  Container,
+  Box,
+  Paper,
+  Card,
+} from "@material-ui/core";
 import MenuItem from "./MenuItem/MenuItem";
 
 import styles from "./Menu.module.css";
 import { useEffect, useState } from "react";
 
 const Menu = () => {
-
   const [menuListArray, setMenuListArray] = useState([]);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     setMenuListArray([
       {
-        MenuItemTitle:"Dango",
-        MenuItemDesc:"a small, sticky, sweet dumpling, commonly skewered on a stick",
-        MenuItemPrice:"30",
-      },
-  
-      {
-        MenuItemTitle:"Kompeito",
-        MenuItemDesc:"A crystal sugar candy",
-        MenuItemPrice:"10",
+        MenuItemTitle: "Dango",
+        MenuItemDesc:
+          "a small, sticky, sweet dumpling, commonly skewered on a stick",
+        MenuItemPrice: "30",
       },
 
       {
-        MenuItemTitle:"Taiyaki",
-        MenuItemDesc:"Fish shaped cake with a sweet filling",
-        MenuItemPrice:"60",
+        MenuItemTitle: "Kompeito",
+        MenuItemDesc: "A crystal sugar candy",
+        MenuItemPrice: "10",
+      },
+
+      {
+        MenuItemTitle: "Taiyaki",
+        MenuItemDesc: "Fish shaped cake with a sweet filling",
+        MenuItemPrice: "60",
       },
     ]);
-  },[])
+  }, []);
 
-  let readyArray=menuListArray.map( menuItem=> (
-    <Box m={1} p={1} key={menuItem.MenuItemTitle} >
-  <MenuItem 
-   MenuItemTitle={menuItem.MenuItemTitle}
-  MenuItemDesc={menuItem.MenuItemDesc}
-  MenuItemPrice={menuItem.MenuItemPrice} />
-   <hr/>
-  </Box>  ));
-
-
-   
-
+  let readyArray = menuListArray.map((menuItem) => (
+    <Box m={1} p={1} key={menuItem.MenuItemTitle}>
+      <MenuItem
+        MenuItemTitle={menuItem.MenuItemTitle}
+        MenuItemDesc={menuItem.MenuItemDesc}
+        MenuItemPrice={menuItem.MenuItemPrice}
+      />
+      <hr />
+    </Box>
+  ));
 
   return (
     <Paper>
-          <Box width="70%"  m={0.5} p={2} mx="10%">
+      <Box width="70%" m={0.5} p={2} mx="10%">
+        <Typography variant="h4" component="h2" align="center">
+          Menu
+        </Typography>
 
-      <Typography variant="h4" component="h2" align="center">Menu</Typography>
-
-      <ul className={styles.MenuList}>
-        {readyArray}
-      </ul>
-
+        <ul className={styles.MenuList}>{readyArray}</ul>
+        <div>
+          Favicon made by{" "}
+          <a
+            href="https://www.flaticon.com/authors/smashicons"
+            title="Smashicons"
+          >
+            Smashicons
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
       </Box>
     </Paper>
-  
-  )
+  );
 };
 
 export default Menu;
