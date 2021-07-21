ae78945f34e7e059f668f1af0b79627962f976c6
algoSupport <algoplusnine@outlook.com>
2021-07-21 20:40:32 +0500

    Added prompts for links

--- src/Ver 2/Components/BottomNav/BottomNav.js
+++ src/Ver 2/Components/BottomNav/BottomNav.js
@@ -1,23 +1,32 @@
-import {BottomNavigation,BottomNavigationAction} from "@material-ui/core";
+import {BottomNavigation,BottomNavigationAction, Dialog} from "@material-ui/core";
 import MobileFriendlyIcon from "@material-ui/icons/MobileFriendly";
 import MobileOffIcon from "@material-ui/icons/MobileOff";
 import { useState } from "react";
-import {Link} from "react-router-dom"
+import {Link,Prompt} from "react-router-dom"
 
 const BottomNav = props => {
 
     const [NavValue, setNavValue] = useState('Ver2')
+    const [isAlert, setIsAlert] = useState(true);
+
 
 return(
+  <>
 <BottomNavigation showLabels value={NavValue}>
+  
   <BottomNavigationAction
     label="Ver1"
     icon={<MobileOffIcon />}
     component={Link}
     to={"/v1"}
   />
+
   <BottomNavigationAction label="Ver2" icon={<MobileFriendlyIcon />} value="Ver2"/>
 </BottomNavigation>
+
+<Prompt when={isAlert} message="Lose data including Cart data?"></Prompt>
+
+</>
 );
 }
 
